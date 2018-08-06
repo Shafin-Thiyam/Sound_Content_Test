92c8933f38238495ceefcb33312a3a80d7a7ceb1
Shafin Thiyam <shafin.thiyam@outlook.com>
2018-08-06 10:22:21 +0530

    Initial version

--- preview_postprocess.py
+++ preview_postprocess.py
@@ -1,11 +1,6 @@
 import os; 
 import sys;
 
-
 for file in sys.argv[1:]:
-    if (str(file).endswith(".aif")):
-        print("{0} {1}.ogg".format(file, file.split(".")[0]))
-        os.system("ffmpeg -i {0}  -ss 00:00:03 -t 00:00:08 -async 1 {1}.ogg".format(file, file.split(".")[0]))
-    else:
-        continue
-
+    print("{0} {1}.ogg".format(file, file.split(".")[0]))
+    os.system("ffmpeg -i {0}  -ss 00:00:03 -t 00:00:08 -async 1 {1}.ogg".format(file, file.split(".")[0]))
