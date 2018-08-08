c8d329b3179d35aee3d2b7645288f1ae84898c74
Shafin Thiyam <shafin.thiyam@thomsonreuters.com>
2018-08-08 21:29:55 +0530

    add threshold, ration and attack

--- preview_postprocess.py
+++ preview_postprocess.py
@@ -5,4 +5,4 @@ for file in sys.argv[1:]:
     file_name=file.split(".")[0]
     print("{0} {1}.ogg".format(file,file.split(".")[0]))
 
-    os.system("ffmpeg -ss 00  -i {0}  -t 12 -af \"afade=t=out:st=11.2:d=1\" {1}.ogg".format(file,file_name))
+    os.system("ffmpeg -ss 00  -i {0}  -t 12 -af \"afade=t=out:st=11.2:d=1,acompressor=threshold=0.1:ratio=2:attack=5\" {1}.ogg".format(file,file_name))
