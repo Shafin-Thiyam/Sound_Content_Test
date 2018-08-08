9408f75cbd530258565b88cb24f4e2262b1085f7
Shafin Thiyam <shafin.thiyam@outlook.com>
2018-08-08 08:09:03 +0530

    Initial version

--- preview_postprocess.py
+++ preview_postprocess.py
@@ -3,4 +3,4 @@ import sys;
 
 for file in sys.argv[1:]:
     print("{0} {1}.ogg".format(file, file.split(".")[0]))
-    os.system("ffmpeg -i {0}  -ss 00:00:03 -t 00:00:08 -async 1 {1}.ogg".format(file, file.split(".")[0]))
+    os.system("ffmpeg -ss 00 -i {0} -t 02  {1}.ogg".format(file, file.split(".")[0]))
