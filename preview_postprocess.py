6eb830c09050a5611c2c59ecf9afe5103739a75c
Shafin Thiyam <shafin.thiyam@thomsonreuters.com>
2018-08-08 21:18:05 +0530

    changes ffmpeg for fading

--- preview_postprocess.py
+++ preview_postprocess.py
@@ -2,5 +2,7 @@ import os;
 import sys;
 
 for file in sys.argv[1:]:
-    print("{0} {1}.ogg".format(file, file.split(".")[0]))
-    os.system("ffmpeg -i {0} -ss 00 -t 12 {1}.ogg".format(file, file.split(".")[0]))
+    file_name=file.split(".")[0]
+    print("{0} {1}.ogg".format(file,file.split(".")[0]))
+
+    os.system("ffmpeg -ss 00  -i {0}  -t 12 -af \"afade=t=out:st=11.2:d=1\" {1}.ogg".format(file,file_name))
